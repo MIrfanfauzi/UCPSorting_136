5271bfd696de54d8d76243841714f8ec6628d5cf
Irfan Fauzi <irfanfauziyk@gmail.com>
2024-03-22 09:29:18 +0700

    Add project files.

---
+++ UCPSorting_136.cpp
@@ -0,0 +1,22 @@
+// Soal tipe B
+// 1. bubble sort
+// - pass= 1
+// -membandingkan elemen dari index 0 dengan index 1
+// - menukarnya jika elemen index 1 lebih besar dari index 0
+// - lanjut ke menbandingkan index 1 dgn index 2
+// - lakukan penukaran jika index 2 lebih besar dari index 1
+// - lakukan lagi sehingga variabel n  habis
+// - ulangi langkah tersebut hingga pass= n-1
+
+// 2. shell sort
+// -
+//
+
+
+#include <iostream>
+
+int main()
+{
+    std::cout << "Hello World!\n";
+}
+
